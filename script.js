c44e7e764fce7f2c2642014d97997fd748a7fe68
oaaj-digitals <124045939+oaaj-digitals@users.noreply.github.com>
2023-02-21 15:40:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuBtn = document.getElementById("menu-icon");
+let menu = document.querySelector(".header-nav");
+let menuBg = document.querySelector(".login-form__div");
+
+let close = (menuBtn, menu, menuBg) => {
+	menuBtn.src = "./icon-menu.svg";
+	menuBtn.classList.remove("opened");
+	menu.style.display = "none";
+	menuBg.style.display = "none";
+	menuBtn.style.position = "static";
+};
+
+let open = (menuBtn, menu, menuBg) => {
+	menuBtn.src = "./icon-menu-close.svg";
+	menuBtn.classList.add("opened");
+	menu.style.display = "block";
+	menuBg.style.display = "block";
+};
+
+menuBtn.addEventListener("click", () => {
+	if (menuBtn.classList.contains("opened")) {
+		close(menuBtn, menu, menuBg);
+		menuBtn.classList.add("closed");
+	} else {
+		open(menuBtn, menu, menuBg);
+	}
+});
